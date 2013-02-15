469132a774e6015cb8056148b01760e40a7337d9
Alumno <Alumno@192.168.158.131>
2013-02-15 08:46:52 +0100

    primera version

---
+++ pruebaClientes/src/Cliente.java
@@ -0,0 +1,43 @@
+
+public class Cliente {
+
+	String nombre;
+	int saldo;
+	
+	
+	//Esto es un constructor
+	public Cliente( String nombreC, int saldoC ){
+		nombre = nombreC;
+		saldo = saldoC;
+		
+		
+	}
+	public void depositar(int deposito) {
+		saldo = saldo + deposito;
+	}
+	public void extraer(int dinero) {
+		saldo = saldo - dinero;
+	}
+	
+	
+	//Este metodo es especial (main) es el principal
+	public static void main(String[] args) {
+		String nombreC = "Juan";
+		int saldoC = 1200;
+		
+		Cliente uno = new Cliente(nombreC, saldoC);
+		
+		System.out.println("He creado a " + uno.nombre + " y tiene un saldo inicial de " + uno.saldo);
+		
+		uno.depositar(1000);
+		
+		System.out.println("Saldo tras deposito " + uno.saldo);
+		
+		uno.extraer(1000);
+		
+		System.out.println("Saldo tras extraccion " + uno.saldo);
+		
+
+	}
+
+}
